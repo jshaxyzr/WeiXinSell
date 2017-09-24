4bf27a00d44756b443c67c16188627279621b4ff
MrZhang <420678364@qq.com>
2017-09-24 20:12:31 +0800

    买家商品Controller

---
+++ src/main/java/cn/MrZhang/controller/SellerProductController.java
@@ -0,0 +1,142 @@
+package cn.MrZhang.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.MrZhang.model.ProductInfo;
+import cn.MrZhang.service.ProductCategoryService;
+import cn.MrZhang.service.ProductInfoService;
+
+/**
+ * 
+ * Title:SellerProductController
+ * @Description: TODO 卖家端商品
+ * @author MrZhang
+ * @date 2017年9月22日 下午5:35:41 
+ * @version V1.0
+ */
+@Controller
+@RequestMapping("/seller/product")
+public class SellerProductController {
+
+    @Autowired
+    private ProductInfoService productInfoService;
+
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
+    /**
+     * 列表
+     * @param page
+     * @param size
+     * @param map
+     * @return
+     */
+    @GetMapping("/list")
+    public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size, Map<String, Object> map) {
+        PageRequest request = new PageRequest(page - 1, size);
+        Page<ProductInfo> productInfoPage = productInfoService.findAll(request);
+        map.put("productInfoPage", productInfoPage);
+        map.put("currentPage", page);
+        map.put("size", size);
+        return new ModelAndView("product/list", map);
+    }
+
+    // /**
+    // * 商品上架
+    // * @param productId
+    // * @param map
+    // * @return
+    // */
+    // @RequestMapping("/on_sale")
+    // public ModelAndView onSale(@RequestParam("productId") String productId, Map<String, Object> map) {
+    // try {
+    // productInfoService.onSale(productId);
+    // } catch (Exception e) {
+    // map.put("msg", e.getMessage());
+    // map.put("url", "/sell/seller/product/list");
+    // return new ModelAndView("common/error", map);
+    // }
+    //
+    // map.put("url", "/sell/seller/product/list");
+    // return new ModelAndView("common/success", map);
+    // }
+    //
+    // /**
+    // * 商品下架
+    // * @param productId
+    // * @param map
+    // * @return
+    // */
+    // @RequestMapping("/off_sale")
+    // public ModelAndView offSale(@RequestParam("productId") String productId, Map<String, Object> map) {
+    // try {
+    // productInfoService.offSale(productId);
+    // } catch (Exception e) {
+    // map.put("msg", e.getMessage());
+    // map.put("url", "/sell/seller/product/list");
+    // return new ModelAndView("common/error", map);
+    // }
+    //
+    // map.put("url", "/sell/seller/product/list");
+    // return new ModelAndView("common/success", map);
+    // }
+    //
+    // @GetMapping("/index")
+    // public ModelAndView index(@RequestParam(value = "productId", required = false) String productId, Map<String, Object> map) {
+    // if (!StringUtils.isEmpty(productId)) {
+    // ProductInfo productInfo = productInfoService.findOne(productId);
+    // map.put("productInfo", productInfo);
+    // }
+    //
+    // // 查询所有的类目
+    // List<ProductCategory> categoryList = categoryService.findAll();
+    // map.put("categoryList", categoryList);
+    //
+    // return new ModelAndView("product/index", map);
+    // }
+    //
+    // /**
+    // * 保存/更新
+    // * @param form
+    // * @param bindingResult
+    // * @param map
+    // * @return
+    // */
+    // @PostMapping("/save")
+    // public ModelAndView save(@Valid ProductForm form, BindingResult bindingResult, Map<String, Object> map) {
+    // if (bindingResult.hasErrors()) {
+    // map.put("msg", bindingResult.getFieldError().getDefaultMessage());
+    // map.put("url", "/sell/seller/product/index");
+    // return new ModelAndView("common/error", map);
+    // }
+    //
+    // ProductInfo productInfo = new ProductInfo();
+    // try {
+    // // 如果productId为空, 说明是新增
+    // if (!StringUtils.isEmpty(form.getProductId())) {
+    // productInfo = productService.findOne(form.getProductId());
+    // } else {
+    // form.setProductId(KeyUtil.genUniqueKey());
+    // }
+    // BeanUtils.copyProperties(form, productInfo);
+    // productService.save(productInfo);
+    // } catch (SellException e) {
+    // map.put("msg", e.getMessage());
+    // map.put("url", "/sell/seller/product/index");
+    // return new ModelAndView("common/error", map);
+    // }
+    //
+    // map.put("url", "/sell/seller/product/list");
+    // return new ModelAndView("common/success", map);
+    // }
+}
