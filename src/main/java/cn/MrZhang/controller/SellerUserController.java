0478bd54965e1d088d4fe5b52dd3d225c4e44816
MrZhang <420678364@qq.com>
2017-09-25 16:04:53 +0800

    登出

--- src/main/java/cn/MrZhang/controller/SellerUserController.java
+++ src/main/java/cn/MrZhang/controller/SellerUserController.java
@@ -3,6 +3,8 @@ package cn.MrZhang.controller;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,20 +61,21 @@ public class SellerUserController {
 
     }
 
-    // @GetMapping("/logout")
-    // public ModelAndView logout(HttpServletRequest request, HttpServletResponse response, Map<String, Object> map) {
-    // // 1. 从cookie里查询
-    // Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
-    // if (cookie != null) {
-    // // 2. 清除redis
-    // redisTemplate.opsForValue().getOperations().delete(String.format(RedisConstant.TOKEN_PREFIX, cookie.getValue()));
-    //
-    // // 3. 清除cookie
-    // CookieUtil.set(response, CookieConstant.TOKEN, null, 0);
-    // }
-    //
-    // map.put("msg", ResultEnum.LOGOUT_SUCCESS.getMessage());
-    // map.put("url", "/sell/seller/order/list");
-    // return new ModelAndView("common/success", map);
-    // }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Map<String, Object> map, Model model) {
+        // 1. 从cookie里查询
+        Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
+        if (cookie != null) {
+            // 2. 清除redis
+            // redisService.removePattern("token_*");//批量删除 token_ 开头的所有key
+            redisService.remove(String.format(RedisConstant.TOKEN_PREFIX, cookie.getValue()));
+            // 3. 清除cookie
+            CookieUtil.set(response, CookieConstant.TOKEN, null, 0);
+        }
+
+        map.put("msg", "登出成功！");
+        map.put("url", "/seller/order/list");
+        model.addAllAttributes(map);
+        return "/common/success";
+    }
 }
